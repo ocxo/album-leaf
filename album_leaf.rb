42586c4e29ffb6d34281a483b39f67884fbe44f6
Adam Ochonicki <ocxo@users.noreply.github.com>
2020-11-25 22:52:44 -0500

    Handle missing metadata

--- album_leaf.rb
+++ album_leaf.rb
@@ -9,30 +9,38 @@ require 'fileutils'
 
 files_processed = 0
 
-Dir.glob('*/*') do |file|
+Dir.glob('*/*/*') do |file|
+  puts "File: #{file}"
   file_type = `file --brief --mime-type '#{file}'`.chomp
   puts "File Type: #{file_type}"
+
+
   if file_type.include?('jpeg')
-    exif_date = EXIFR::JPEG.new(file).date_time
-    puts "EXIF Date: #{exif_date}"
+    jpeg = EXIFR::JPEG.new(file)
+    if jpeg.exif? and jpeg.date_time
+      exif_date = jpeg.date_time
+      puts "EXIF Date: #{exif_date}"
 
-    date = exif_date.strftime("%Y-%m-%d")
-    Dir.mkdir(date) unless File.exists?(date)
+      date      = exif_date.strftime("%Y-%m-%d")
+      date_time = exif_date.strftime("%Y-%m-%d-%H-%M-%S%z")
+      file_name = "#{date_time}.jpg"
 
-    date_time = exif_date.strftime("%Y-%m-%d-%H-%M-%S%z")
-    file_name = "#{date_time}.jpg"
-    dest_file_path = "./#{date}/#{file_name}"
+      Dir.mkdir(date) unless File.exists?(date)
 
-    if File.exists?(dest_file_path)
-      file_name = "#{date_time}-duplicate-#{files_processed}.jpg"
       dest_file_path = "./#{date}/#{file_name}"
+
+      if File.exists?(dest_file_path)
+        file_name = "#{date_time}-duplicate-#{files_processed}.jpg"
+        dest_file_path = "./#{date}/#{file_name}"
+      end
+
+      FileUtils.mv(file, dest_file_path) unless File.exists?(dest_file_path)
     end
-      
-    FileUtils.mv(file, dest_file_path) unless File.exists?(dest_file_path)
+
     puts "Wrote: #{dest_file_path}"
     files_processed += 1
 
     puts "Done: #{files_processed}"
-    puts
   end
+  puts
 end
